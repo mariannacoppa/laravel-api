ddb6f344a0b076f19c6825c2992b057dccb1fbe5
mariannacoppa <densimetra@gmail.com>
2024-10-19 14:50:33 +0200

    [UPD] added validation

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -13,6 +13,23 @@ class LeadController extends Controller
         // importo i dati dalla form
         $form_data = $request->all();
         // implemento la validazione
-        
+        $validator = Validator::make($form_data, [
+            'name' => ['required', 'max:50'],
+            'surname' => ['required', 'max:100'],
+            'email' => ['required', 'max:150'],
+            'phone' => ['required', 'max:20'],
+            'content' => ['required'],
+        ],
+        $errors = [
+            'name.required' => 'Il nome è obbligatorio',
+            'name.max' => 'Il nome non può superare i :max caratteri',
+            'surname.required' => 'Il cognome è obbligatorio',
+            'surname.max' => 'Il cognome non può superare i :max caratteri',
+            'email.required' => 'L\'indirizzo email è obbligatorio',
+            'email.max' => 'L\'indirizzo email non può superare i :max caratteri',
+            'phone.required' => 'Il numero di telefono è obbligatorio',
+            'phone.max' => 'Il numero di telefono non può superare i :max caratteri',
+            'content.required' => 'Il contenuto della mail è obbligatorio',
+        ]);
     }
 }
